0b0f23e69afd60b6ba987bc783e9a929452b89e5
AparnaMudunuri <aparnam.528@gmail.com>
2019-12-24 06:09:29 -0500

    updated

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ import config
 
 import pymysql
 
+import os
+import ibm_boto3
+from ibm_botocore.client import Config, ClientError
+import json
+import requests
 
 # In[ ]:
 
@@ -57,7 +62,32 @@ def show_first():
 #def show_index(select_key):
 #    return render_template("index.html", user_image = url_dict['URL'])
 
+cos_service_credentials = {
+    "apikey": "_bAzHuCAN1yPz4Rcg5CZY1Tbp0UOpshuMhpoNkIvJAa3",
+    "endpoints": "https://control.cloud-object-storage.cloud.ibm.com/v2/endpoints",
+    "iam_apikey_description": "Auto-generated for key b766a2b2-aacd-4a78-aaed-1784769a82a6",
+    "iam_apikey_name": "gamification-cos-standard-tkq",
+    "iam_role_crn": "crn:v1:bluemix:public:iam::::serviceRole:Writer",
+    "iam_serviceid_crn": "crn:v1:bluemix:public:iam-identity::a/693fe8ead49b44b192004113d21b15c2::serviceid:ServiceId-f6d85b01-d45a-4d92-831d-3e3efa44bb3c",
+    "resource_instance_id": "crn:v1:bluemix:public:cloud-object-storage:global:a/693fe8ead49b44b192004113d21b15c2:fce26086-5b77-42cc-b1aa-d388aa2853d7::"
+}
 
+endpoints = requests.get(cos_service_credentials['endpoints']).json()
+iam_host = (endpoints['identity-endpoints']['iam-token'])
+cos_host = (endpoints['service-endpoints']['regional']['us-south']['public']['us-south'])
+auth_endpoint = "https://" + iam_host + "/oidc/token"
+service_endpoint = "https://" + cos_host
+
+cos = ibm_boto3.resource("s3",
+    ibm_api_key_id=cos_service_credentials['apikey'],
+    ibm_service_instance_id=cos_service_credentials['resource_instance_id'],
+    ibm_auth_endpoint=auth_endpoint,
+    config=Config(signature_version="oauth"),
+    endpoint_url=service_endpoint
+)
+
+
+        
 
 @app.route('/',methods=['POST','GET'])
 def my_form_post():
@@ -69,6 +99,9 @@ def my_form_post():
     retrive = f"Select * from XXIBM_PRODUCT_SKU where DESCRIPTION LIKE '%{url_key}%' LIMIT 1;"
     cursor.execute(retrive)
     rows = cursor.fetchall()
+    
+
+
     
     if len(rows) != 0:
         url_dict = rows[0]
@@ -83,9 +116,23 @@ def my_form_post():
     
     print(select_key)
         
+    def get_item(bucket_name, item_name):
+    print("Retrieving item {0} from bucket {1}".format(item_name, bucket_name))
+    try:
+        obj = cos.Object(bucket_name, item_name).get()
+        v2= obj['Body'].read()
+        return obj['Body'].read()
+    except ClientError as be:
+        print("CLIENT ERROR: {0}\n".format(be))
+    except Exception as e:
+        print("Unable to retrieve file contents: {0}".format(e))
+        
+    bucket_name = "gamification-cos-standard-tkq"
+    item_name = "1001.jpg"
+    
+    img = get_item(bucket_name, item_name)
 
-
-    return render_template("index.html", user_image = url_dict['DESCRIPTION'])
+    return render_template("index.html", user_image = url_dict['img'])
 
 
 # In[ ]:
