45a9d3e6e621ad1184e850bf057c0d6202676019
AparnaMudunuri <aparnam.528@gmail.com>
2019-12-24 01:19:40 -0500

    updated

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def my_form_post():
         
 
 
-    return render_template("index.html", user_image = url_dict['URL'])
+    return render_template("index.html", user_image = url_dict['u'])
 
 
 # In[ ]:
