c5b3971c3e22ce8d955559b4a42f09bb5779c86d
AparnaMudunuri <aparnam.528@gmail.com>
2019-12-24 01:01:11 -0500

    changed

--- app.py
+++ app.py
@@ -5,17 +5,20 @@
 
 
 from flask import Flask,render_template,request,redirect,url_for
+
+import config
+
 import pymysql
 
 
 # In[ ]:
 
 
-host="database-2.contk8ieomwk.ap-south-1.rds.amazonaws.com"
+host="custom-mysql.gamification.svc.cluster.local"
 port=3306
-dbname="loblaw"
-user="salai123"
-password="RF&cloudy11"
+dbname="sampledb"
+user="xxuser"
+password="welcome1"
 
 conn = pymysql.connect(host, user=user,port=port,
                            passwd=password, db=dbname,cursorclass=pymysql.cursors.DictCursor)
@@ -36,7 +39,7 @@ app = Flask(__name__)
 # In[ ]:
 
 
-retrive = f"Select * from ITEM where NAME LIKE '%jac%' LIMIT 1;"
+retrive = f"Select * from XXIBM_PRODUCT_SKU where DESCRIPTION LIKE '%jac%' LIMIT 1;"
 cursor.execute(retrive)
 rows = cursor.fetchall()
 len(rows)
@@ -63,20 +66,20 @@ def my_form_post():
     text = request.form['u']
     url_key = text.lower()
     
-    retrive = f"Select * from ITEM where NAME LIKE '%{url_key}%' LIMIT 1;"
+    retrive = f"Select * from XXIBM_PRODUCT_SKU where DESCRIPTION LIKE '%{url_key}%' LIMIT 1;"
     cursor.execute(retrive)
     rows = cursor.fetchall()
     
     if len(rows) != 0:
         url_dict = rows[0]
-        select_key = url_dict['NAME']
+        select_key = url_dict['DESCRIPTION']
         
     else:
-        retrive = "Select * from ITEM where NAME='none';"
+        retrive = "Select * from XXIBM_PRODUCT_SKU where DESCRIPTION ='none';"
         cursor.execute(retrive)
         rows = cursor.fetchall()
         url_dict = rows[0]
-        select_key = url_dict['NAME']
+        select_key = url_dict['DESCRIPTION']
     
     print(select_key)
         
@@ -89,8 +92,7 @@ def my_form_post():
 
 
 if __name__ == "__main__":
-    app.run(port=8080)
-    conn.close()
+    app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
 
 
 # In[ ]:
